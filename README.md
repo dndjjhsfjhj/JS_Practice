72078de7ba36d5b57916aba025e828a8ce1e2e3d
dndjjhsfjhj <92665212+dndjjhsfjhj@users.noreply.github.com>
2023-02-03 00:17:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS_Practice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-au4hrc)
---
+++ script.js
@@ -0,0 +1,78 @@
+// // // let obj={name:"komal"
+// // // }
+// // // let obj1={name1:"yogita"}
+// // //  let obj3={...obj , ...obj1}
+// // //  console.log(obj3);
+
+// // // //  1.function argument
+
+// // // let food=["banana","orange","mango","papaya"];
+// // // function Food(a,b,c,d){
+// // // console.log(a,b,c,d);
+// // // }
+// // // Food(...food)
+// //  function Rest(...a){
+// //    console.log("komal",a)
+// //  }
+// //  Rest("banana","orange","mango","papaya")
+
+// // //  spread=function calling argument
+// // //  rest=function defination argument
+// // // console.log(this)=undefined
+
+// // let Number=[1,2,3,4,5,6,7,8,9,10];
+// // let x=Number.map((el)=>el*2);
+// // console.log(x)
+
+// // function Hello(){
+// //   console.log("Hello world");
+// // }
+// // Hello()
+// var a=1;
+// var a=2;
+// console.log(a)
+// let b=1;
+// let b=2;
+// console.log(b)
+// const c=1;
+// const c=2;
+// console.log(c);
+// // scope,reassign,redecalre...(let,var,const)
+var a=1;
+function foo(){
+  var b=2;
+  console.log(b);
+}
+console.log(a);
+foo();
+// {}=>block scope...
+// var=global and functional scope...
+// const and let=write in block scope..
+
+let Plus=()=>console.log("Hello WQorld");
+Plus()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
